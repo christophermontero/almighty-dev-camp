358be88c7e2ea315b795c08de3172a63e235c39a
christophermontero <chris.to20@hotmail.com>
2021-06-15 14:51:09 -0500

    Set up controller to bootcamps route

--- server.js
+++ server.js
@@ -1,30 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// Routes
+const bootcamps = require('./routes/bootcamps');
+
 // load env vars
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-app.get('/api/v1/bootcamps', (req, res) => {
-  res.json({ success: true, msg: 'Show all bootcamps' });
-});
-
-app.get('/api/v1/bootcamps/:id', (req, res) => {
-  res.json({ success: true, msg: `Show bootcamp ${req.params.id}` });
-});
-
-app.post('/api/v1/bootcamps', (req, res) => {
-  res.json({ success: true, msg: 'Create new bootcamp' });
-});
-
-app.put('/api/v1/bootcamps/:id', (req, res) => {
-  res.json({ success: true, msg: `Update bootcamp ${req.params.id}` });
-});
-
-app.delete('/api/v1/bootcamps/:id', (req, res) => {
-  res.json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
-});
+// Mount routes
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 3000;
 

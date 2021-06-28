d049e3494a2a420e9cf4b8b9e107027256b0072f
christophermontero <chris.to20@hotmail.com>
2021-06-28 09:45:15 -0500

    Filtering logic from get all bootcamps is extracted

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -10,13 +10,19 @@ const {
   bootcampPhotoUpload
 } = require('../controllers/bootcamps');
 
+const Bootcamp = require('../models/Bootcamp');
+const advancedResults = require('../middleware/advancedResults');
+
 // Include other resource routers
 const courseRouter = require('./courses');
 
 // Re-route into other resource routers
 router.use('/:bootcampId/courses', courseRouter);
 
-router.route('/').get(getBootcamps).post(createBootcamp);
+router
+  .route('/')
+  .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
+  .post(createBootcamp);
 
 router
   .route('/:id')

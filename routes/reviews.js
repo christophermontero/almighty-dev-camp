4c4c5fa367972eee46b4c9a5bfcc02bd4772b180
christophermontero <chris.to20@hotmail.com>
2021-07-15 10:37:00 -0500

    Routes for update and delete reviews are created

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,19 +1,30 @@
 const express = require('express')
 
 const router = express.Router({ mergeParams: true })
-const { getReviews, getReview, createReview } = require('../controllers/reviews')
+const {
+  getReviews,
+  getReview,
+  createReview,
+  updateReview,
+  deleteReview
+} = require('../controllers/reviews')
 
 const Review = require('../models/Review')
 const advancedResults = require('../middleware/advancedResults')
 
 const { protect, authorize } = require('../middleware/auth')
 
-router.route('/').get(advancedResults(Review, {
-  path: 'bootcamp',
-  select: 'name description'
-}), getReviews)
+router.route('/').get(advancedResults(Review,
+  {
+    path: 'bootcamp',
+    select: 'name description'
+  }
+), getReviews)
   .post(protect, authorize('user', 'admin'), createReview)
 
-router.route('/:id').get(getReview)
+router.route('/:id')
+  .get(getReview)
+  .put(protect, authorize('user', 'admin'), updateReview)
+  .delete(protect, authorize('user', 'admin'), deleteReview)
 
 module.exports = router
